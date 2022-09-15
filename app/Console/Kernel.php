e10ae8c4f49667f443f75107a898a0607c14bd80
Dimanok1989 <kolgaev@gmail.com>
2022-09-15 16:11:20 +0300

    Включение планировщика

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('checkpayfine')
+            ->dailyAt("23:59")
+            ->runInBackground();
     }
 
     /**
@@ -25,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
