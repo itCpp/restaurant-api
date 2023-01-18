2d59e983f150209f459dafd53426e4f91355bc06
Dimanok1989 <kolgaev@gmail.com>
2023-01-18 20:50:39 +0300

    корректировка остатков

--- app/Http/Controllers/Cashbox/Info.php
+++ app/Http/Controllers/Cashbox/Info.php
@@ -17,6 +17,10 @@ class Info extends Controller
     public function index(Request $request)
     {
         CashboxTransaction::selectRaw('sum(sum) sum, IFNULL(type_pay, 1) type_pay')
+            ->where(function ($query) {
+                $query->where('type_pay', null)
+                    ->orWhereIn('type_pay', [1, 4]);
+            })
             ->where('date', '>=', "2022-10-01")
             ->groupBy('type_pay')
             ->get()
@@ -37,11 +41,39 @@ class Info extends Controller
                     $data[$key]['sum'] += 185069.88;
             });
 
+        CashboxTransaction::selectRaw('sum(sum) sum, IFNULL(type_pay, 1) type_pay')
+            ->where(function ($query) {
+                $query->where('type_pay', '!=', null)
+                    ->orWhereNotIn('type_pay', [1, 4]);
+            })
+            ->where('date', '>=', "2023-01-01")
+            ->groupBy('type_pay')
+            ->get()
+            ->each(function ($row) use (&$data) {
+
+                $key = (int) $row->type_pay;
+
+                if (!isset($data[$key])) {
+                    $data[$key] = [
+                        'sum' => 0,
+                        'type_pay' => $key,
+                    ];
+                }
+
+                $data[$key]['sum'] += $row->sum;
+
+                if ($key == 3)
+                    $data[$key]['sum'] += 93260.16;
+            });
+
         return response()->json([
-            'info' => collect($data ?? [])->map(function ($row) {
-                $row['sum'] = round($row['sum'], 2);
-                return $row;
-            })->values()->all()
+            'info' => collect($data ?? [])
+                ->map(function ($row) {
+                    $row['sum'] = round($row['sum'], 2);
+                    return $row;
+                })
+                ->values()
+                ->all()
         ]);
     }
 }
